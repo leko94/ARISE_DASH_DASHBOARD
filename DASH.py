b95620872d60dfb1838d79c7da69724ffcb0ca84
leko94 <ngcobo.nkululeko@yahoo.com>
2026-07-19 15:59:15 +0200

    Update Excel file name in DASH.py

--- DASH.py
+++ DASH.py
@@ -9,7 +9,7 @@ import plotly.graph_objects as go
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Excel file name can be overridden in Render Env Vars:
-# EXCEL_FILE = Stokvel_20260415.xlsx
+# EXCEL_FILE = Stokvel_20260719.xlsx
 EXCEL_FILE = os.getenv("EXCEL_FILE", "Stokvel_10072026.xlsx")
 EXCEL_PATH = os.path.join(BASE_DIR, EXCEL_FILE)
 
